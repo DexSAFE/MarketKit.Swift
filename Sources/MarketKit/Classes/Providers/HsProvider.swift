c08a4442b90ab9fd05245fb31eacb1c092c57c91
DexSAFE <dexsafe.github@gmail.com>
2024-07-25 11:54:29 +0300

    revert base url HSProvider

--- Sources/MarketKit/Classes/Providers/HsProvider.swift
+++ Sources/MarketKit/Classes/Providers/HsProvider.swift
@@ -82,8 +82,7 @@ extension HsProvider {
     }
 
     func allBlockchainRecords() async throws -> [BlockchainRecord] {
-        try await networkManager.fetch(url: "https://bridge.dexnetchain.com/image/blockchains.json", method: .get, headers: headers())
-//        try await networkManager.fetch(url: "\(baseUrl)/v1/blockchains/list", method: .get, headers: headers())
+        try await networkManager.fetch(url: "\(baseUrl)/v1/blockchains/list", method: .get, headers: headers())
     }
 
     func allTokenRecords() async throws -> [TokenRecord] {
